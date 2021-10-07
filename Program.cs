70ac6d64738f6bd9f7bce7137cba1dcb8d1840a1
David Riano Marin <davidrmarin1@gmail.com>
2021-10-07 18:01:17 -0500

    Falta conectar las funciones con las demas clases

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace localproject
+{
+    class Person
+    {
+        static void Main(string[] args)
+        {
+            // Atributes 
+            string name;
+            string last_name;
+            string user_id;
+            string phone_number;
+            string gender;
+            string password;
+
+            Console.ReadKey();
+        }
+
+
+        static void consult_variables()
+        {
+
+  
+        }
+        
+        static void consult_history()
+        {
+
+
+        }
+
+
+
+    }
+}
