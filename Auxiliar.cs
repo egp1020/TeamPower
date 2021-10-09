5f0cb52ae34634051451a1e442c1b209687a3681
David Riano Marin <davidrmarin1@gmail.com>
2021-10-09 02:28:42 -0500

    falta relacion con otras clases

---
+++ Auxiliar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace program 
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            // Attributes
+            int auxiliar ;
+        }
+        // Missing Connections to the other classes
+        static void register_shed()
+        {
+
+        }
+
+        static  void register_vet()
+        {
+
+        }
+
+        static void register_operator()
+        {
+
+        }
+
+        static void consult_shed()
+        {
+
+        }
+
+        static void consult_specific_shed(shed_id)
+        {
+
+        }
+
+        static void assing_vet_to_shed(id_galpon, id_vet)
+        {
+
+        }
+
+        static void assing_operator(operator_id, shed_id)
+        {
+
+        }
+    }
+}
